35c2f5d65cc96d657c9a072feb6ded97bec0595e
Vicky Le <vle04@student.rtc.edu>
2024-04-18 17:38:10 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Prog120_S24_L6_Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
